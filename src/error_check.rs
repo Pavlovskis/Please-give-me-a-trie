38baae68b74a6cfd6203e0daebbff0da6ca67102
Pavlovskis <129526327+Pavlovskis@users.noreply.github.com>
2025-02-27 13:26:16 +0000

    Update error_check.rs

--- src/error_check.rs
+++ src/error_check.rs
@@ -12,12 +12,12 @@ impl ErrorCheckable for Trie {
 
         let mut res:Vec<String> = Vec::with_capacity(4);
 
-        let edits:[fn(&Trie, word:&str) -> Vec<String>; 4] = [
+        let edits:[fn(&Trie, word:&str, distance:usize) -> Vec<String>; 4] = [
             Self::deletion, Self::transposition, Self::alteration, Self::insertion 
         ];
 
         for edit in edits {
-            res.append( &mut edit(&self, word) );
+            res.append( &mut edit(&self, word, 1) );
         }
 
         if res.is_empty() { return None; }
@@ -33,14 +33,14 @@ trait ErrorCheckEdits {
     // Note that if a string s has a deletion distance of 1 from another string t then |s| = |t| -1. 
     // Also, there are exactly | t | strings that are a deletion distance of 1 from t.
     // The dictionary may contain 0 to n of the strings one deletion distance from t .
-    fn deletion(&self, word:&str) -> Vec<String>;
+    fn deletion(&self, word:&str, distance:usize) -> Vec<String>;
 
     // A string s has a transposition distance 1 from another string t if and only if t is equal to s with two adjacent 
     // characters transposed. The only strings that are a transposition Distance of 1 from “house” are “ohuse”, “huose”, 
     // “hosue” and “houes”. Note that if a string s has a transposition distance of 1 from another string t then |s| = |t|. 
     // Also, there are exactly | t | - 1 strings that are a transposition distance of 1 from t . 
     // The dictionary may contain 0 to n of the strings one transposition distance from t .
-    fn transposition(&self, word:&str) -> Vec<String>;
+    fn transposition(&self, word:&str, distance:usize) -> Vec<String>;
 
     // A string s has an alteration distance 1 from another string t if and only if t is equal to s with exactly one character 
     // in s replaced by a lowercase letter that is not equal to the original letter. The only strings that are an alternation 
@@ -48,65 +48,61 @@ trait ErrorCheckEdits {
     // Note that if a string s has an alteration distance of 1 from another string t then |s| = |t|. 
     // Also, there are exactly 25* | t | strings that are an alteration distance of 1 from t . 
     // The dictionary may contain 0 to n of the strings one alteration distance from t .
-    fn alteration(&self, word:&str) -> Vec<String>;
+    fn alteration(&self, word:&str, distance:usize) -> Vec<String>;
 
     // A string s has an insertion distance 1 from another string t if and only if t has a deletion distance of 1 from s . 
     // The only strings that are an insertion distance of 1 from “ask” are “aask”, “bask”, “cask”, … “zask”, “aask”, “absk”, “acsk”, … 
     // “azsk”, “asak”, “asbk”, “asck”, … “aszk”, “aska”, “askb”, “askc”, … “askz”. Note that if a string s has an insertion 
     // distance of 1 from another string t then |s| = |t|+1. Also, there are exactly 26* (|t|+1) strings that are an insertion 
     // distance of 1 from t . The dictionary may contain 0 to n of the strings one insertion distance from t .
-    fn insertion(&self, word:&str) -> Vec<String>;
+    fn insertion(&self, word:&str, distance:usize) -> Vec<String>;
 }
 
 impl ErrorCheckEdits for Trie {
     
-    fn deletion(&self, word:&str) -> Vec<String> {
-        let distance = 1;
+    fn deletion(&self, word:&str, distance:usize) -> Vec<String> {
         let mut res:Vec<String> = Vec::with_capacity(4);
 
-        // let mut cur:NodeRef = self.root.clone();
-
-        for i in 0..word.len() - distance + 1 {
-            let new_word:String = format!("{}{}", &word[..i], &word[i + distance..]);
+        for i in 0..word.len() - distance as usize + 1 {
+            let new_word:String = word[..i].to_string() + &word[i + distance as usize..];
             if self.contains(&new_word) { res.push(new_word); }
         }
         res
     }
 
-    fn transposition(&self, word:&str) -> Vec<String> {
-        let mut distance = 2;
+    fn transposition(&self, word:&str, mut distance:usize) -> Vec<String> {
         if distance == 1 { distance += 1; }
 
         let mut res:Vec<String> = Vec::with_capacity(4);
 
-        for i in 0..word.len() - distance + 1 {
-            let mut chars_slice:Vec<char> = word[i..i + distance].chars().collect();
+        'outer: for d in 2..distance + 1 {
+            for i in 0..word.len() - d + 1 {
+                let mut chars_slice:Vec<char> = word[i..i + d].chars().collect();
             
-            let permutations:Vec<String> = permutations(&mut chars_slice);
-
-            for perm in permutations {
-                let new_word:String = format!("{}{}{}", &word[0..i], perm, &word[i+distance..] );
-                println!("{}", new_word);
-                if self.contains(&new_word) { res.push(new_word); }
+                let permutations:Vec<String> = permutations(&mut chars_slice);
+    
+                for perm in permutations {
+                    let new_word:String = word[0..i].to_string() + &perm + &word[i + d..];
+                    if self.contains(&new_word) { 
+                        res.push(new_word); 
+                        break 'outer;
+                    }
+                }
             }
         }
         res
     }
 
-    fn alteration(&self, word:&str) -> Vec<String> {
+    //todo
+    fn alteration(&self, word:&str, _distance:usize) -> Vec<String> {
         let mut res:Vec<String> = Vec::with_capacity(word.len());
         
         for (i, _) in word.char_indices() {
             let (left, right) = ( &word[0..i], &word[i + 1..] );
             
             for n in 97..123 as u8 {
-                let mut new_word:String = String::with_capacity(word.len() + 1);
-            
-                new_word.push_str(left);
-                new_word.push(n as char);
-                new_word.push_str(right);
+                let new_word:String = left.to_string() + &n.to_string() + right;
 
-                print!("{},", new_word);
                 if self.contains(&new_word) { res.push(new_word);}
             }
             println!()
@@ -114,7 +110,8 @@ impl ErrorCheckEdits for Trie {
         res
     }
 
-    fn insertion(&self, word:&str) -> Vec<String> {
+    //todo
+    fn insertion(&self, word:&str, _distance:usize) -> Vec<String> {
         let mut res:Vec<String> = Vec::with_capacity(16);
 
         for (i, _) in word.char_indices() {
@@ -126,16 +123,12 @@ impl ErrorCheckEdits for Trie {
                 new_right.push_str(right);
 
                 let new_word = left.to_owned() + &new_right;
-                if self.contains(&new_word) {
-                    res.push(new_word);
-                }
+                if self.contains(&new_word) { res.push(new_word); }
             }
         }
         res
     }
 }
-
-
 
 fn permutations(chars:&mut Vec<char>) -> Vec<String> {
     let n:usize = chars.len();
